f057617084170a840aba65ca10d7c1ba3f00407b
Antonello03 <antoplay03@gmail.com>
2023-10-15 10:30:43 +0200

    funcanimation testing, ready to start?

--- project/main.py
+++ project/main.py
@@ -1,85 +1,58 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import time
 
-""" Implementation of the Conway's Game of Life """
-
-def grid_init(N):
-    """ Returns an empty NxN grid """
+def init_grid(N):
+    """Initialize the NxN grid with a Glider and a LWSS."""
     grid = np.zeros((N, N), dtype=int)
+
+    # Glider
+    glider = np.array([[0, 255, 0],
+                       [0, 0, 255],
+                       [255, 255, 255]])
+    grid[1:4, 1:4] = glider
+
+    # Lightweight Spaceship (LWSS)
+    lwss = np.array([[255, 255, 255, 255, 0],
+                     [0, 0, 0, 0, 255],
+                     [255, 0, 0, 0, 255],
+                     [0, 255, 255, 255, 255]])
+    grid[10:14, 10:15] = lwss
+
     return grid
 
-def plot_update(grid):
 
-    plt.imshow(grid, cmap='viridis', interpolation='nearest')
-
-
-def update(frame):
-    """ Update function for the animation """
-    global grid
-    grid = grid_evolve(grid)
-    im.set_array(grid)
-    return im,
-
-
-def grid_evolve(grid, N):
-    """ Return the next generation grid """
-    # evolution of a generation
-    next = grid_init(N)
-    for row in range(0, N):
-        for col in range(0, N):
-            # compute the # neighbors of [row][col] cell
-            neigh = 0
-            for i in range(max(0, row-1), min(N, row+2)):
-                # exploring the neighborhood in row direction
-                for j in range(max(0, col-1), min(N, col+2)):
-                    # exploring the neighborhood in col direction
-                    neigh += grid[i][j]                
-            neigh -= grid[row][col]
-            
-            # compute the status of the [row][col] cell in the next generation
-            if grid[row][col] == 1:
-                # live cell
-                if 2 <= neigh <=3:
-                    next[row][col] = 1
-                # otherwise it is already empty
+def update(frameNum, img, grid, N):
+    """Update the grid for animation."""
+    newGrid = grid.copy()
+    for i in range(N):
+        for j in range(N):
+            total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] +
+                         grid[(i-1)%N, j] + grid[(i+1)%N, j] +
+                         grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
+                         grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N]) / 255)
+            if grid[i, j]  == ON:
+                if (total < 2) or (total > 3):
+                    newGrid[i, j] = OFF
             else:
-                # dead cell
-                if neigh == 3:
-                    next[row][col] = 1
+                if total == 3:
+                    newGrid[i, j] = ON
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return img,
 
-    return next
+# Parameters
+N = 50  # Grid size
+ON = 255  # Alive cell
+OFF = 0  # Dead cell
+UPDATE_INTERVAL = 0  # in milliseconds
+grid = init_grid(N)
 
-#### MAIN ####
-
-# data structure
-# A matrix of integers
-#  0: dead cell
-#  1: live cell
-
-# initialization of a NxN empty grid
-N = 20 # size of the grid (N x N)
-grid = grid_init(N)
-
-steps = 10 # duration of the simulation
-
-#print(grid)
-
-# blinker
-grid[1][2] = 1
-grid[2][2] = 1
-grid[3][2] = 1
-grid[2][0] = 1
-grid[3][1] = 1
-
-
-
-for s in range(1, steps+1):
-    # next generation
-    print('time:', s)
-    grid = grid_evolve(grid, N)
-    plot_update(grid)
-    plt.pause(0.01)
+# Create the plot
+fig, ax = plt.subplots()
+img = ax.imshow(grid, interpolation='nearest', cmap='gray')
+ani = FuncAnimation(fig, update, fargs=(img, grid, N, ),
+                    interval=UPDATE_INTERVAL,
+                    save_count=50)
 
 plt.show()
--- project/test.py
+++ project/test.py
@@ -1,27 +1,22 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from math import *
+
+fig, ax = plt.subplots()
+x, y = [], []
+line, = plt.plot([], [], 'bo-')
 
 def init():
-    # Initialization code (if any)
-    pass
-
-def update(frame):
-    # Update code based on the frame number
-    # For example, update a line plot
-    line.set_ydata(np.sin(x + frame * 0.1))
+    ax.set_xlim(0, 10*pi)
+    ax.set_ylim(-1, 1)
     return line,
 
-# Create a figure and axes
-fig, ax = plt.subplots()
+def update(frame):
+    x.append(frame/10)
+    y.append(sin(frame/10))
+    line.set_data(x, y)
+    return line,
 
-# Generate x data
-x = np.linspace(0, 2 * np.pi, 100)
+ani = FuncAnimation(fig, update, init_func=init, blit=True, interval=5)
 
-# Initialize a line plot
-line, = ax.plot(x, np.sin(x))
-
-# Create the animation
-ani = FuncAnimation(fig, update, frames=100, init_func=init, interval=50, blit=True)
-
-# Show the plot
 plt.show()
