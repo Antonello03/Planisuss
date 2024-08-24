c510f32eac55f755fc5b64a787e33096b6e02034
Antonello03 <antoplay03@gmail.com>
2024-08-24 21:17:05 +0200

    world islands and water generation with a thing called fbm

--- project/interface.py
+++ project/interface.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.colors import LinearSegmentedColormap
 
 from world import Environment
 from planisuss_constants import *
@@ -18,15 +19,27 @@ OFF = 0
 
 def update(frameNum, img):
     """Update the grid for animation."""
-    newGrid = env.updateEnv()
-    img.set_data(newGrid)
+    #newGrid = env.updateEnv()
+    #img.set_data(newGrid)
     return img,
 
 grid = env.getGrid()
 
 # Create the plot
-fig, ax = plt.subplots()
-img = ax.imshow(grid, interpolation='nearest', cmap='gray')
+fig, ax = plt.subplots(figsize=(5, 5))
+
+# this is just to see how it would appear, The final mechanism will be more complex I think
+colors = [(0.0, "blue"), (1.0, "green")]
+cmap_name = "blue_to_brown"
+custom_cmap = LinearSegmentedColormap.from_list(cmap_name, colors)
+
+img = ax.imshow(grid, interpolation='nearest', cmap = custom_cmap)
+
+# better window appearance
+ax.axis('off')
+fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
+fig.canvas.manager.set_window_title("Planisuss World")
+
 ani = FuncAnimation(fig, update, fargs=(img,),
                     interval=1,
                     save_count=50)
--- project/world.py
+++ project/world.py
@@ -1,6 +1,7 @@
 import random
 from planisuss_constants import *
 import numpy as np
+import noise
 
 ON = 255
 OFF = 0
@@ -20,7 +21,6 @@ class Environment():
     
     def updateEnv(self):
         N = NUMCELLS
-        
         # Evolution of the world logic
         newGrid = self.world.grid.copy()
         for i in range(N):
@@ -48,30 +48,59 @@ class WorldGrid():
 
     its main attribute is grid
     """
-    def randomGrid(gridEL, percentage=0.5):
+    def randomGrid(self, gridEL, percentage=0.5):
         ''' Stupid fucntion tu generate ranom initial population'''
-
         if random.random() < percentage:
             return 255
         else:
             return 0
     v_randomGrid = np.vectorize(randomGrid)
 
-    def __init__(self, type = "islands"):
+    def __fbmNoise(self, n, threshold = 0.4, seed = None, octaves=8, persistence=0.4, lacunarity=1.8, scale=40.0):
+        """ Method to generate island like maps """
+        if seed is None:
+            seed = random.randint(0, 100)
+        grid = np.zeros((n, n))
+        for i in range(n):
+            for j in range(n):
+                x = i / scale
+                y = j / scale
+                fbm_value = 0.0
+                amplitude = 1.0
+                frequency = 1.0
+                for _ in range(octaves):
+                    fbm_value += amplitude * noise.pnoise2(x * frequency, y * frequency, base = seed)
+                    amplitude *= persistence
+                    frequency *= lacunarity
+                
+                grid[i][j] = fbm_value
+
+        # Normalize the values between 0 and 1
+        min_val = np.min(grid)
+        max_val = np.max(grid)
+        grid = (grid - min_val) / (max_val - min_val)
+        print(grid)
+        grid = np.where(grid > threshold, 255, 0)
+        print(grid)
+        
+        return grid
+
+    def __init__(self, type = "fbm"):
         self.grid = self.createWorld(type)
         
     # so that we can crate different types of initial setups
-    def createWorld(self, typology = "islands"):
-        if typology == "islands":
-            self.grid = np.zeros(shape=(NUMCELLS,NUMCELLS))
-            self.grid = self.v_randomGrid(self.grid, 0.3)
-            return self.grid
+    def createWorld(self, typology = "fbm"):
+        if typology == "random":
+            grid = np.zeros(shape=(NUMCELLS,NUMCELLS))
+            grid = self.v_randomGrid(self.grid, 0.3)
+            return grid
         
-        if typology == "type1":
-            pass
+        if typology == "fbm":
+            grid = self.__fbmNoise(NUMCELLS)
+            return grid
 
         if typology == "type2":
             pass
 
-    def updateGrid(self,newGrid):
+    def updateGrid(self, newGrid):
         self.grid = newGrid
