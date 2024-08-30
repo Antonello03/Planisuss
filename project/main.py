e4dfa890c6212e8485db5483f9bc91c0df480807
Stefania <stefaniarosci2007@gmail.com>
2024-08-30 21:03:30 +0200

    updated interface

--- project/main.py
+++ project/main.py
@@ -3,7 +3,7 @@ from world import *
 from creatures import *
 
 environment = Environment()
-print(environment.grid[50][50].getVegetobDensity())
+# print(environment.grid[50][50].getVegetobDensity())
 
 animation = Interface(env = environment)
 
