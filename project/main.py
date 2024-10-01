7f9f71f538aea6144a9fd230d9e87fd665b88e3c
Antonello03 <antoplay03@gmail.com>
2024-10-01 13:45:26 +0200

    merge

--- project/main.py
+++ project/main.py
@@ -6,6 +6,9 @@ from pprint import pprint
 environment = Environment()
 animation = Interface(env = environment)
 
+def printInfo():
+    print(f"GridCell\nErbasts: {gridCell.creatures["Erbast"]}\nnumErbast:{gridCell.numErbast}\nherd: {gridCell.herd}\n\nEnvironment\ntotErb: {environment.totErbast}\nerbasts: {environment.creatures["Erbast"]}\nherds:{environment.getHerds()}\naloneErbasts: {environment.getAloneErbasts()}\n\n")
+
 grid = environment.getGrid()
 gridCell = environment.getGrid()[25][25]
 
@@ -20,18 +23,13 @@ erbOther2 = Erbast((24,24))
 herd1 = Herd([erb1,erb2,erb3])
 herd2 = Herd([erbOther, erbOther2])
 
-# for a in animals:
-#     environment.add(a)
+carv1 = Carviz((23,23))
 
-gridCell = environment.getGrid()[25][25]
 environment.add(herd1)
 environment.add(herd2)
-
-carv1 = Carviz((24, 24))
-carv2 = Carviz((24, 24))
 environment.add(carv1)
-environment.add(carv2)
 
+animation.start()
 
 # print(environment.creatures["Erbast"])
 # for comp in environment.creatures["Erbast"]:
@@ -40,22 +38,7 @@ environment.add(carv2)
 # pprint(herd1.rankMoves(grid))
 # pprint(erb1.rankMoves(grid))
 
-# tot_erbasts = environment.creatures['Erbast']
-# for erbast in tot_erbasts:
-#    print(f"coordinates of the erbast: {erbast.getCoords()}")
 
 # for i in range(4):
 #     environment.nextDay()
-
-
-# print(environment.creatures)
-
-# for i in range(5):
-#     print(f"\nday {i}:")
-#     for animal in es:
-#         print(f"{animal} is in {animal.getCoords()} and it desired to move in")
-#         pprint(animal.rankMoves(grid)[:3])
-#     environment.nextDay()
-
-animation.start()
 
